2e7e76c51271156cf670d7c78c38825b2bf36f88
Angelo Pedrosa <angelo.ppedrosa@gmail.com>
2022-05-27 17:47:45 -0300

    configurações inicias finalizadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import * as C from "./App.styles"
-
+import {MainRoutes} from './routes/mainRoutes'
 const App = () => {
  
   return(
     <C.Container>
-      
+        <MainRoutes/>
     </C.Container>
   )
 
