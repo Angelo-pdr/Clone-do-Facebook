5b8baeda7eb321868517f8a065d9bd255553c797
Angelo Pedrosa <angelo.ppedrosa@gmail.com>
2022-05-27 18:14:19 -0300

    feito area de descrição

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,6 +5,15 @@ export const Home = () => {
 
     return(
         <C.Container>
+            <C.Description>
+                <a href="/"><img src="../src/img/logoFacebook.svg" alt="logo_do_facebook" className="imagem" /></a>
+                <h2>
+                O Facebook ajuda você a se conectar e compartilhar com as pessoas que fazem parte da sua vida.
+                </h2>
+            </C.Description>
+            <C.FormArea>
+
+            </C.FormArea>
         </C.Container>
     )
 }
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -1,5 +1,34 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    padding: 20px 0;
+    margin: 0 184.5px;
+    background-color: transparent;
+    display: grid;
+    grid-template-columns: 1.5fr 1fr;
     
+`
+
+export const Description = styled.div`
+    
+
+    .imagem{
+        height: 106px;
+        margin: -26px;
+        padding: 112px 0 16px;
+       
+    }
+
+    h2{
+        max-width: 500px;
+        font-size: 28px;
+        font-weight: normal;
+        line-height: 32px;
+        color: #1c1e21;
+        margin: 0;
+    }
+`
+
+export const FormArea = styled.div`
+
 `
